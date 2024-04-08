8a0bbe07b779c2f6147aaf548e2c3675e229233f
Jacob Petterle <jacobpetterle@gmail.com>
2024-04-08 02:19:49 +0000

    updated project config

--- .projenrc.ts
+++ .projenrc.ts
@@ -3,16 +3,18 @@ import { NodePackageManager } from 'projen/lib/javascript';
 
 const project = new typescript.TypeScriptProject({
   defaultReleaseBranch: 'main',
-  name: 'name',
-  description: 'A cool typescript project',
+  name: 'Portfolio Site for Jacob Petterle',
+  description: 'My personal portfolio site',
   projenrcTs: true,
   projenVersion: '0.80.19',
   autoMerge: true,
   autoMergeOptions: {},
+  testdir: '.', // we don't have tests in the top level
   license: 'MIT',
   copyrightOwner: 'Jacob Petterle',
   devDeps: ['eslint@9.0', 'nx@^18.2', 'typescript@^5.4'],
   packageManager: NodePackageManager.PNPM,
+  github: true,
   pnpmVersion: '8.15.6',
   disableTsconfig: true,
   eslintOptions: {
@@ -35,4 +37,8 @@ const project = new typescript.TypeScriptProject({
 });
 
 project.gitignore.exclude('.pnpm-store', '.nx', '.next');
+
 project.synth();
+// remove src directory forcefully using typescript file system
+const fs = require('fs');
+fs.rmdirSync('src', { recursive: true });
